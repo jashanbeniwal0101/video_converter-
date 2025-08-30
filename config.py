fd559855dcd2a78b97e2522c99b05af062239ac7
jashanbeniwal0101 <beniwaljass733@gmail.com>
2025-08-30 14:41:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID", "25331263"))
 API_HASH = os.getenv("API_HASH", "cab85305bf85125a2ac053210bcd1030")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7368321164:AAFNU9jp-x1qsb_s6VviTpiMRxkVHKwDHB4")
-
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1002888391802"))
 OWNER_ID = int(os.getenv("OWNER_ID", "1955406483"))
 
 # Working directory
