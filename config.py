fa059bd8f8dcbaeac24790f64002d98acc631d92
jashanbeniwal0101 <beniwaljass733@gmail.com>
2025-08-30 13:02:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv('API_ID', '0'))
-API_HASH = os.getenv('API_HASH', '')
-BOT_TOKEN = os.getenv('BOT_TOKEN', '')
-OWNER_ID = int(os.getenv('OWNER_ID', '0'))
+API_ID = int(os.getenv('API_ID', '25331263'))
+API_HASH = os.getenv('API_HASH', 'cab85305bf85125a2ac053210bcd1030')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '7368321164:AAFNU9jp-x1qsb_s6VviTpiMRxkVHKwDHB4')
+OWNER_ID = int(os.getenv('OWNER_ID', '1955406483'))
 
 WORKDIR = Path(os.getenv('WORKDIR', './runtime')).resolve()
 WORKDIR.mkdir(parents=True, exist_ok=True)
